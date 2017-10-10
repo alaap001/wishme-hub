0533c9a1db35cb1c7bdecd6ee137a782c1ba21b9
Alaap Dhall <alaapdhall79@gmail.com>
2017-10-10 08:13:52 +0530

    more fn added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,16 +16,26 @@
 //= require turbolinks
 //= require_tree .
 
+
 $(document).ready(function() {
-$(document).on('click','#wisher_btn',function(){
-  //e.preventDefault();
-  
-   var doc = document.getElementById("wisher_name");
+
+	function clickfn(){
+		 var doc = document.getElementById("wisher_name");
 
     var diwali_wisher = doc.value;
  	var url1 = "https://floating-hamlet-93576.herokuapp.com/occasions/diwali/"+diwali_wisher; 
  	var url2 = "http://localhost:3000/occasions/diwali/"+diwali_wisher;
   location.href = url1;
 
+	}
+$(document).on('click','#wisher_btn',function(){
+  //e.preventDefault();
+  clickfn();
+});
+$(document).keypress(function(e) {
+    if(e.which == 13) {
+        clickfn();
+        e.preventDefault();
+    }
 });
 });
